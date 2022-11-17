13df8417cdff8491d8467b394f5d3fe7546a7318
adamrexroade <arexroade@wisc.edu>
2022-11-17 10:10:56 +0930

    changed plot themes, started adding discharge data

--- USGSDATA.R
+++ USGSDATA.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(dplyr)
 library(lubridate)
 library(patchwork)
+library(dataRetrieval)
 
 df <- read_csv("Data/USGSSynoptic.csv") %>% 
   mutate(YEAR=year(Date)) %>% 
@@ -11,13 +12,15 @@ df <- read_csv("Data/USGSSynoptic.csv") %>%
 #time Series
 c <- ggplot(df, aes(Date, `CO2_umoles/L_syringe`, colour=Site))+
   geom_line()+
-  theme(legend.position = "top")+
-  ylim(50,220)
+  ylim(50,220)+
+  theme_bw()+
+  theme(legend.position = "top")
   
 d <- ggplot(df, aes(Date, `CH4_umoles/L_syringe`, colour=Site))+
   geom_line()+
-  theme(legend.position = "top")+
-  ylim(0,5)
+  ylim(0,5)+
+  theme_bw()+
+  theme(legend.position = "top")
 
 c+d+plot_layout(ncol=1)
 
@@ -32,7 +35,8 @@ a <- ggplot(df, aes(Site, `CO2_umoles/L_syringe`))+
                             "Allequash Wetland near tower",
                             "Allequash Creek Synoptic 4",
                             "ALLEQUASH CREEK SITE NO. 3 NR BOULDER JUNCTION, WI"))+
-  ylim(0,210)
+  ylim(0,210)+
+  theme_bw()
 
 
 b <- ggplot(df, aes(Site, `CH4_umoles/L_syringe`))+
@@ -45,7 +49,26 @@ b <- ggplot(df, aes(Site, `CH4_umoles/L_syringe`))+
                             "Allequash Wetland near tower",
                             "Allequash Creek Synoptic 4",
                             "ALLEQUASH CREEK SITE NO. 3 NR BOULDER JUNCTION, WI"))+
-  ylim(0,10)
+  ylim(0,10)+
+  theme_bw()
 
 a+b+plot_layout(ncol = 1)
+
+# look at discharge
+
+Q<-readNWISdata(sites=c("05357205",  "05357206"), parameterCd =c("00060"), 
+                startDate = "2018-10-01", endDate = "2021-11-10", service="iv") %>% 
+  mutate(Date=date(dateTime)) %>% 
+  mutate(Year=year(Date)) %>% 
+  mutate(Time=time(dateTime)) %>% 
+  mutate(Discharge=X_00060_00000/35.315) %>% 
+  select(Date, Year, site_no, Time, Discharge)
+q_sum <- Q %>% 
+  group_by(Date, site_no) %>% 
+  summarise(`Discharge (m3/sec)`=mean(Discharge)) %>% 
+  mutate(Year=as.factor(year(Date))) %>% 
+  mutate(Day=yday(Date))
+
+q_long <- pivot_longer(q_sum, cols = c(`Discharge (m3/sec)`)
+
 
